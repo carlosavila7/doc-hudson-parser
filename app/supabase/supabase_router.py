99fade4cb762424943ba9f972c78b68ca11c8dba
Carlos Avila <carlos_ed03@outlook.com>
2025-11-20 01:00:30 -0300

    feat: implement endpoint to download file

--- app/supabase/supabase_router.py
+++ app/supabase/supabase_router.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, File, UploadFile, APIRouter
+from fastapi import File, UploadFile, APIRouter
+from fastapi.responses import StreamingResponse
 from .supabase_service import SupabaseService
 from typing import Annotated
+import io
 
 router = APIRouter(
     prefix="/supabase",
@@ -30,13 +32,30 @@ async def get_files_by_bucket(
 
     return service.get_files_from_bucket(bucket)
 
+@router.get("/download-file/{bucket}")
+def download_file(
+    bucket: str,
+    path: str
+):
+    """Endpoint to download a file from Supabase S3."""
+    service: SupabaseService = get_storage_service()
+
+    bytes = service.download_file_from_s3(bucket, path)
+
+    return StreamingResponse(
+            content=io.BytesIO(bytes),
+            headers={
+                "Content-Disposition": f"attachment; filename={path.split('/')[-1]}"
+            }
+        )
+
 @router.post("/upload-file/{bucket}")
-async def get_files_by_bucket(
+async def upload_file(
     bucket: str,
     path: str,
     file: Annotated[UploadFile, File()]
 ):
-    """Endpoint to retrieve a file from Supabase S3."""
+    """Endpoint to upload a file to Supabase S3."""
     service: SupabaseService = get_storage_service()
 
     return await service.upload_file_to_s3(bucket, path, file)
--- app/supabase/supabase_service.py
+++ app/supabase/supabase_service.py
@@ -7,20 +7,19 @@ class SupabaseService:
     def __init__(self):
         self.client = get_supabase_client()
 
-    # 1. Access files from S3
     def download_file_from_s3(self, bucket_name: str, file_path: str) -> bytes:
         """Downloads a file from Supabase Storage (S3)."""
-        # Note: Supabase uses bucket/path, not just path
         res = self.client.storage.from_(bucket_name).download(file_path)
         return res
 
+
     # 2. Post data to PostgreSQL
-    def insert_data_to_postgres(self, table_name: str, data: List[Dict[str, Any]]):
-        """Inserts processed data into a PostgreSQL table."""
-        # Using the postgrest client accessible via .table()
-        res = self.client.table(table_name).insert(data).execute()
-        # res.data will contain the inserted row data on success
-        return res.data
+    # def insert_data_to_postgres(self, table_name: str, data: List[Dict[str, Any]]):
+    #     """Inserts processed data into a PostgreSQL table."""
+    #     # Using the postgrest client accessible via .table()
+    #     res = self.client.table(table_name).insert(data).execute()
+    #     # res.data will contain the inserted row data on success
+    #     return res.data
 
     async def upload_file_to_s3(self, bucket: str, path: str, upload_file: UploadFile):
         content = await upload_file.read()
